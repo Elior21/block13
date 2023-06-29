9b48c6cbbf97b9bc819ea967644217f5284a3f28
elior valenzuela <eliorvalenzuela@gmail.com>
2023-06-29 10:02:12 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,69 @@
+// pseudocode make a prompt
+// pseudocode if false state "The boolean value false is falsy"
+// pesudocode if null state "The null value is falsy";
+
+//const input = prompt(`are you a string`)
+
+/*if (input === `i am a string`){
+    console.log(`true`)
+} else if(input === `false`) {
+    console.log(`the boolean value is false is falsy`);
+} else if (input === `null`) {
+    console.log(`the null value is falsy`);
+} else if (input === `undefined`) {
+    console.log(`undefined is falsy`)
+} else if (input === `0`){
+    console.log(`the number 0 is falsy`)
+} else if (input === `""`) {
+    console.log(`the empty string is falsy`)
+}*/
+
+//pseduocode create a script tag that takes the sum of two numbers
+//pseduocode sum + is greater
+//pseduocode sum + is equal 
+//pseduocode sum + is negative
+//pseduocode sum + is less
+
+
+/*const inputOne = prompt (`enter first number`)
+const num1 = Number(inputOne)
+
+const inputTwo = prompt(`enter the second number`)
+const num2 = Number(inputTwo)
+
+const sum = num1 + num2
+
+if(sum === -1000){
+    console.log(`-1000 is a negative number`)
+}else if(sum === -5) {
+    console.log(`-5 is a negative number`)
+}else if(sum === 0) {
+    console.log(`0 is equal to 0`)
+}else if (sum> 100) {
+    console.log (`${sum} greater than 100`)
+}*/
+
+
+//pseduocode create a script tag 
+const input1 = 5
+const input2 = 4
+//pseduocode prints true if both parameter is greater to 5
+//pseduocode print true if both parameter is equal to 5
+if(input1 >= 5 && input2 >= 5){
+    console.log(true)
+}
+//pseduocode print false if one input is less than 5
+else if(input1 < 5 || input2 < 5){
+    console.log(false)
+}
+
+
+/*const input1 = 5
+const input2 = 4
+
+if(input1>= 5 && input2>= 5){
+    console.log(`true`)
+}else if(input1<= 5 && input2<= 5) {
+    console.log(`false`)
+}*/
+
